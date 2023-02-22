cb390f3bf45be59a68052ae4f4c83eb2c41b6bdd
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-02-22 16:09:09 +0530

    Time: 5 ms (32.48%), Space: 5.9 MB (93.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    // run a loop 32 times
+    // extract the i'th bit starting from last
+    // make ans variable and initialize it to 0
+    //
     uint32_t reverseBits(uint32_t n) 
     {
         uint32_t ans=0;
         for(int i=0;i<32;i++)
-        {
-            bool x=n&(1<<i);
-            if(x)
+            if(n&(1<<i))
                 ans^=(1<<(31-i));            
-        }
         return ans;
     }
 };
