d0b2346b149155351ca977a316abb790edc4cf56
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-02-22 16:10:14 +0530

    Time: 6 ms (31.36%), Space: 6 MB (76.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     // run a loop 32 times
-    // extract the i'th bit starting from last
+    // extract the i'th bit of n starting from last
     // make ans variable and initialize it to 0
-    //
+    // if extracted bit is  then toggle the ans's bit to  using XOR
+    
     uint32_t reverseBits(uint32_t n) 
     {
         uint32_t ans=0;
