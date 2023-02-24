a69b629b296579f496974ad091404a52a463c95a
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-02-24 17:47:45 +0530

    Time: 8 ms (30.87%), Space: 6.9 MB (28.60%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string s) {
-        vector<int> ans;
+    // s =          "ababcbacadefegdehijhklij"
+    //               ↑ 
+    // look at this "a" (ask here ki piche se [last se] 'a' konsi index per he)
+    // iska answer map se milega , map will contain this information ki ye
+    // letter piche se konsi index per appear hua he
+    
+   
+        
+        
+    vector<int> partitionLabels(string s) 
+    {
         unordered_map<char,int> m;
         for(int i=0;i<s.size();i++)
             m[s[i]]=i;
+        
+        // p represents partition index, c represents length of partition
+        // p ko jitna dur bhej skte ho
+        // bhejo, thats why we are, maximising p, or jab i==p ho jayega it means
+        // current partition ke sare log mil ke bus p ko itna hi dur 
+        // fek paye to yahi per ans banega or next partition start hoga        
         int p=0,c=0;
+        vector<int> ans;
         for(int i=0;i<s.size();i++)
         {
             if(m[s[i]]>p)
