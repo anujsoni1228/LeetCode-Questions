614f9c87ac35dc682fccbb74e11be0365dc4e45a
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-02-25 23:46:07 +0530

    Time: 136 ms (8.44%), Space: 69.7 MB (8.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    
+// See for every number in making our target sum, we have two possibility i.e
+
+// 1. Whether to include that element in making our target sum.
+// 2. Whether not to include that element in making our target sum.
+        
+// So, if we do not a include a element in our answer then without decreasing target sum, we will move to next index. 
+// Why we do not decrese sum? 
+//because we will choose not to include in this element in our combination, hence it does not contribute in making our sum.
+
+// But, if we choose a particular element to include in our answer, then we will decrease the target sum but we will not move to next index. 
+// Why we will not move to next index?
+// because for a specific element we have unlimited number of choice, so it may be possible that specific element again contribute in making our sum.
+    
+    //  for detailed explaination and image 
+    // see pdf i have made
+    // link https://leetcode.com/problems/combination-sum/discuss/1777334/C%2B%2BorDetailed-Explanation-w-TREE-DIAGRAMor-RECURSION-%2B-BACKTRACKINGor-EACH-STEP-EXPLAINED
+    
+    
+    
     vector<vector<int>> ans;
     void helper(vector<int>& c, int t,vector<int> bag,int i)
     {
@@ -12,7 +32,6 @@ public:
     vector<vector<int>> combinationSum(vector<int>& c, int t) 
     {
         vector<int> bag;
-        sort(c.begin(),c.end());
         helper(c,t,bag,0);
         return ans;
     }
