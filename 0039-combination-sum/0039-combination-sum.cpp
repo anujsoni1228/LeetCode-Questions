c502b313669fad875584a8e91a152decd224e9a2
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-02-25 23:24:32 +0530

    Time: 121 ms (10.85%), Space: 69.7 MB (8.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,12 +3,12 @@ public:
     void helper(vector<int>& c, int t,vector<int> bag,vector<vector<int>>& ans,int i)
     {
         if(t<0 || i>=c.size()) return;
-        if(0==t)  { ans.push_back(bag); return; }        
-        vector<int> x=bag;
-        x.push_back(c[i]);
-        helper(c,t-c[i],x,ans,i);
+        if(0==t)  { ans.push_back(bag); return; }       
         helper(c,t,bag,ans,i+1);
+        bag.push_back(c[i]);
+        helper(c,t-c[i],bag,ans,i);        
     }
+    
     vector<vector<int>> combinationSum(vector<int>& c, int t) 
     {
         vector<int> bag;
