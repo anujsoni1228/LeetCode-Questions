4a35be9e4c30343417fe509657b1f1032e9854ed
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-02 15:26:28 +0530

    Time: 16 ms (21.01%), Space: 10.1 MB (59.90%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-//     int helper(vector<vector<int>>& grid, int i, int j) {
-//         if(grid.size()-1==i and grid[0].size()-1==j)
-//             return grid[i][j];
+    // plain recursion
+    int helper1(vector<vector<int>>& grid, int i, int j) {
+        if(grid.size()-1==i and grid[0].size()-1==j)
+            return grid[i][j];
         
-//         int x=INT_MAX,y=INT_MAX;
-//         if(j+1<grid[0].size())
-//             y=helper(grid,i,j+1);
-//         if(i+1<grid.size())
-//             x=helper(grid,i+1,j);        
-//         return grid[i][j]+min(x,y);
-//     }
-//     int minPathSum(vector<vector<int>>& grid) {
-//         return helper(grid,0,0);
-//     }
+        int x=INT_MAX,y=INT_MAX;
+        if(j+1<grid[0].size())
+            y=helper1(grid,i,j+1);
+        if(i+1<grid.size())
+            x=helper1(grid,i+1,j);        
+        return grid[i][j]+min(x,y);
+    }
+    int minPathSum1(vector<vector<int>>& grid) {
+        return helper1(grid,0,0);
+    }
  
     
-//     int helper(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp) {
-//         if(grid.size()-1==i and grid[0].size()-1==j)
-//             return dp[i][j]=grid[i][j];
+    // recursion with 2D dp[][]
+    int helper2(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp) {
+        if(grid.size()-1==i and grid[0].size()-1==j)
+            return dp[i][j]=grid[i][j];
         
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         
-//         int x=INT_MAX,y=INT_MAX;
-//         if(j+1<grid[0].size())
-//             y=helper(grid,i,j+1,dp);
-//         if(i+1<grid.size())
-//             x=helper(grid,i+1,j,dp);        
-//         return dp[i][j]=grid[i][j]+min(x,y);
-//     }
-//     int minPathSum(vector<vector<int>>& grid) {
-//         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-//         return helper(grid,0,0,dp);
-//     }
+        int x=INT_MAX,y=INT_MAX;
+        if(j+1<grid[0].size())
+            y=helper2(grid,i,j+1,dp);
+        if(i+1<grid.size())
+            x=helper2(grid,i+1,j,dp);        
+        return dp[i][j]=grid[i][j]+min(x,y);
+    }
+    int minPathSum2(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        return helper2(grid,0,0,dp);
+    }
     
     
+    // tabulation 
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n));
