fbf64039381b9697b0b01e03e20f94c7faf15e5d
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-03 23:09:01 +0530

    Time: 33 ms (74.02%), Space: 10.7 MB (72.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,8 +4,17 @@ public:
     {
         int r=a.size(),c=b.size();
         int dp[r+1][c+1];
+        
+        // filling 0's to last row and last coloumn
         for(int i=0;i<=r;i++)dp[i][c]=0;
         for(int i=0;i<=c;i++)dp[r][i]=0;
+        
+        // if both characters match then go 1+
+        //                                     ↘   {diagonally right-bottom}
+        // if not then take max( ,⇒)  {adjacent right & bottom}
+        //                      ⇓
+        
+        // BOTTOM UP [DP] 
         for(int i=r-1;i>=0;i--)
         {
             for(int j=c-1;j>=0;j--)
@@ -19,3 +28,4 @@ public:
         return dp[0][0];
     }
 };
+
