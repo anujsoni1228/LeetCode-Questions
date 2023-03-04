4b9850991f259614dbb5fae44309b607b3c7e1fe
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-04 16:56:02 +0530

    Time: 2 ms (45.54%), Space: 6.5 MB (56.73%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    // ¯\_( ͡❛ ͜ʖ ͡❛)_/¯
+    //     |✪| 
+    //     // \\
+    //   MY OWN APPROACH 
+    
+    // if the upcoming character is alphabet [abcd..]
+    // append to result (as it is)
+    // if it is a number (may be single or multiple digit no. like 144)
+    // store it in t = t*10 + no. (accumulating it)
+    // if '[' comes then extract the whole part untill the 
+    // corresponding ']' comes and then send this extracted part
+    // again to decodeStrings(); [Recursion] and then paste it's decoded version's
+    // copies t number of times to the result
+    
+    // firstly i used a stack but then i converted the stack into
+    // a normal int variable
+    
     string decodeString(string s) 
     {
         string res;
