4764cb7da284af5ba736232cd20914e8a468c84e
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-08 22:53:19 +0530

    Time: 7 ms (97.67%), Space: 8.6 MB (77.59%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
-    vector<int> findA4nagrams(string s, string p) 
+    vector<int> findAnagrams(string s, string p) 
     {
+        // calculate the lengths of each string
         int m=s.size();
         int n=p.size();
+
+        // if p's length is more than s (do nothing)
+        if(n>m)
+            return {};
+
+        // build two hash sets 
+        // 1. for p (contain frequencies of each character in p) 
+        // 2. for s (contain frequencies of each character in current window)
         int h[26]={0};
         int ph[26]={0};
+        
+        // for ans
         vector<int> ans;
+
+        //count the frequencies of first match
+        // e.g. - "abc" and "cba"-"ebabacd" have been counted
         for(int i=0;i<n;i++)
             ph[p[i]-'a']++,h[s[i]-'a']++;
         
+        // sliding window approach
         for(int i=0;i+n-1<m;)
         {
             bool f=0;
+            // chck for beinig anagram (by comparing frequencies of char)
+            // if atleast a single value doest match then its not our ans
             for(int j=0;j<26;j++)
                 if(ph[j]!=h[j])
                     {f=1;break;}
+
+            // if it's ans then store it
             if(!f)
                 ans.push_back(i);
+
+            // decrease the frequencies of char removed from window
             h[s[i]-'a']--;
+            // i++ means slide the window to right
             i++;
+            // (edge case handle + increase the count 
+            // of char being included to window)
+            if(s[i+n-1]!='\0')
             h[s[i+n-1]-'a']++;            
         }
         return ans;
     }
-    vector<int> findAnagrams(string s, string p) {
-        int s_len = s.length();
-        int p_len = p.length();
-        
-        if(s.size() < p.size()) return {};
-        
-        vector<int> freq_p(26,0);
-        vector<int> window(26,0);
-        
-        //first window
-        for(int i=0;i<p_len;i++){
-            freq_p[p[i]-'a']++;
-            window[s[i]-'a']++;
-        }
-        
-        vector<int> ans;
-        if(freq_p == window) ans.push_back(0);
-        
-        for(int i=p_len;i<s_len;i++){
-            window[s[i-p_len] - 'a']--;
-            window[s[i] - 'a']++;
-            
-            if(freq_p == window) ans.push_back(i-p_len+1);
-        }
-        return ans;
-    }
+
 };
