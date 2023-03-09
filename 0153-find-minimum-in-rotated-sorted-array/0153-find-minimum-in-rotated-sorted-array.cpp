98ca2f5813aa5fc98237db6796ffaa81518cfb48
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-09 22:20:38 +0530

    Time: 7 ms (19.19%), Space: 10.1 MB (79.80%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
+    // same logic lagaya he 
+    // kisi bhi element ke agal bagal wale 2 subarrays me se ek to sorted hoga hi, atleast ek to sorted hoga hi maximum dono bhi sorted ho skte he
+    
+    int findMin(vector<int>& nums) 
+    {
         int n=nums.size();
         int l=0,h=n-1;
+        
+        // edge case
         if(n==1)
             return nums[0];
+        // edge case
         else if(n==2)
             return nums[nums[0]>nums[1]];
+        
+        // binary srch
         while(l<=h)
         {
+            // if the whole array is sorted then the leftmost is the minimum
             if(nums[l]<nums[h])
                 return nums[l];
+            
+            // else find mid and identify which part is sorted
             else
             {
                 int mid=l+(h-l)/2;
+                // if mid is the ans (then it's previous element will be >> than it)
                 if(mid-1>=0 and nums[mid]<nums[mid-1])
                     return nums[mid];
                 else
                 {
+                    // if left part is sorted (go to right)
                     if(nums[l]<=nums[mid])
                         l=mid+1;
+                    // if right part is sorted (go to left)
                     else
                         h=mid-1;
                 }
