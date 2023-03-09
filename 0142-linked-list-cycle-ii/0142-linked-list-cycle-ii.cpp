55c106b8cb32a899045acf1ac66b2cbbd3d31e5d
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-09 23:29:08 +0530

    Time: 12 ms (43.98%), Space: 7.6 MB (68.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -27,6 +27,32 @@ public:
         return NULL;
     }
     
+    
+//     We have discussed Floyd’s loop detection algorithm. Below are steps to find the first node of the loop.
+// 1. If a loop is found, initialize a slow pointer to head, let fast pointer be at its position. 
+// 2. Move both slow and fast pointers one node at a time. 
+// 3. The point at which they meet is the start of the loop.
+
+// We can conclude below from the above diagram 
+
+// Distance traveled by fast pointer = 2 * (Distance traveled 
+//                                          by slow pointer)
+
+// (m + n*x + k) = 2*(m + n*y + k)
+
+// Note that before meeting the point shown above, fast
+// was moving at twice speed.
+
+// x -->  Number of complete cyclic rounds made by 
+//        fast pointer before they meet first time
+
+// y -->  Number of complete cyclic rounds made by 
+//        slow pointer before they meet first time
+// From the above equation, we can conclude below 
+
+//     m + k = (x-2y)*n
+
+// Which means m+k is a multiple of n.
     ListNode *detectCycle(ListNode *head) {
         ListNode *s=head,*f=head;
         while(f && f->next)
