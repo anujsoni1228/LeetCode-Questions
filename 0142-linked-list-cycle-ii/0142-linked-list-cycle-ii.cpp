73697f2b33dae0e866383ea8d7a9bbc8c52049cc
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-09 23:21:01 +0530

    Time: 6 ms (90.60%), Space: 7.7 MB (29.86%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//     O(N) time O(1) Space
+//     But it is modifying the linked list
     ListNode *detectCycle1(ListNode *head) {
         while(head)
         {
@@ -10,7 +12,10 @@ public:
         }
         return NULL;
     }
-    ListNode *detectCycle(ListNode *head) {
+    
+//     O(N) time O(N) Space
+//     But it is Using O(N) Linear space
+    ListNode *detectCycle2(ListNode *head) {
         unordered_set<ListNode*> s;
         while(head)
         {
@@ -21,4 +26,23 @@ public:
         }
         return NULL;
     }
+    
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *s=head,*f=head;
+        while(f && f->next)
+        {
+            s=s->next;
+            f=f->next->next;
+            if(s==f)
+                break;
+        }
+        if(f==NULL || f->next==NULL)
+            return NULL;
+        while(head!=f)
+            head=head->next,f=f->next;
+        return f;
+    }
+    
+    
+    
 };
