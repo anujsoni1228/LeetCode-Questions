b2681a8f5a13632fab0328579db35f70c7620c74
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-09 22:34:01 +0530

    Time: 14 ms (26.57%), Space: 9.5 MB (22.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
+    ListNode *detectCycle1(ListNode *head) {
         while(head)
         {
             if(head->val==-53141)
                 return head;
             head->val=-53141;
+            head=head->next;
+        }
+        return NULL;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode*> s;
+        while(head)
+        {
+            if(s.find(head)!=s.end())
+                return head;
+            s.insert(head);
             head=head->next;
         }
         return NULL;
