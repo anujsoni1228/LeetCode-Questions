d0e9962d5b9d697ebb23fff63a752634405bc468
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-20 16:33:39 +0530

    Time: 16 ms (77.89%), Space: 20.4 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,8 +5,9 @@ public:
         bool l,r;
         for(int i=0;i<n;i++)
         {
-            l=i==0?0:nums[i-1];
-            r=i==n-1?0:nums[i+1];
+            l = (i==0)   ? 0 : nums[i-1] ;
+            r = (i==n-1) ? 0 : nums[i+1] ;
+            
             if(l==0 and r==0 and nums[i]==0)
                 N--,nums[i]=1;
         }    
