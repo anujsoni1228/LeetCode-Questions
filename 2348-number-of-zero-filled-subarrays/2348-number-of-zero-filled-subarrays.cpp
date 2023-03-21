9e89c7662e96e63598eaba27dd1c927c606f33c7
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-21 16:51:03 +0530

    Time: 168 ms (75.72%), Space: 107.6 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
+    long long zeroFilledSubarray(vector<int>& nums) 
+    {
         int n=nums.size();
         long long ans=0,c;
         for(int i=0;i<n;i++)
         {
             c=0;
+            // count no. of continuous zeroes
             while(i<n and nums[i]==0)
                 c++,i++;
+            // sum of n natural numbers formula used
             ans+=c*(c+1)/2;
         }
         return ans;
     }
-};
+    
+    
+    // if c=6, means 6 contaguous zeroes are there
+    // so [ 0,0,0,0,0,0 ]
+    
+    // 1 length wale = 6 [0]
+    // 2 length wale = 5 [0,0]
+    // 3 length wale = 4.. [0,0,0]
+    //     and so on..
+    // it means 6 + 5 + 4 + 3 + 2 + 1 will be total subarrays
+}; 
