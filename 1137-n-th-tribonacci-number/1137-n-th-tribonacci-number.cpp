1d67f698343190250f183a6d0c8029884c783371
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-22 15:46:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.59%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,28 +2,42 @@ class Solution {
 public:
     
     
-    int tribonacci1(int n) 
+//     int tribonacci1(int n) 
+//     {
+//         if(n==0 || n==1)
+//             return n;
+//         if(n==2)
+//             return 1;
+//         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+//     }
+        
+//     int tribonacci(int n) 
+//     {
+//         vector<int> dp(n+1,-1);    
+//         return helper(n,dp);
+//     }
+//     int helper(int n,vector<int> &dp) 
+//     {
+//         if(n==0 or n==1)
+//             return n;
+//         if(n==2)
+//             return 1;
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+//     }
+    
+    int tribonacci(int n) 
     {
         if(n==0 || n==1)
             return n;
         if(n==2)
             return 1;
-        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        vector<int> dp(n+1);
+        dp[1]=1,dp[2]=1;
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        return dp[n];
     }
-        
-    int tribonacci(int n) 
-    {
-        vector<int> dp(n+1,-1);    
-        return helper(n,dp);
-    }
-    int helper(int n,vector<int> &dp) 
-    {
-        if(n==0 or n==1)
-            return n;
-        if(n==2)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
-    }
+    
 };
