5cd50509d22a417fa9571f48268f6be7f488f07f
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-22 14:49:35 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fib1(int n) {
         if(n==0 || n==1)
             return n;
         return fib(n-1)+fib(n-2);
     }
+    
+    int help(int n,vector<int> &dp)
+    {
+        if(n==0 || n==1)
+            return dp[n]=n;
+        if(dp[n]!=-1)
+            return dp[n];
+        else
+            return dp[n]=help(n-1,dp)+help(n-2,dp);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return help(n,dp);
+    }
+    
 };
