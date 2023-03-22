2c80013c2094f7bebb6e0d4d7bebfaf16b68fdc2
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-22 15:49:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.15%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -29,10 +29,6 @@ public:
     
     int tribonacci(int n) 
     {
-        if(n==0 || n==1)
-            return n;
-        if(n==2)
-            return 1;
         vector<int> dp(4);
         dp[1]=1,dp[2]=1;
         for(int i=3;i<=n;i++)
