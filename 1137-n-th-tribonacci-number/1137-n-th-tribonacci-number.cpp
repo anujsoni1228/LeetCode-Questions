4a51ac705229318723bba814d304a6eeea14b2c9
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-22 15:51:14 +0530

    Time: 3 ms (41.82%), Space: 6 MB (40.59%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     
+    // recursion plain
+    int tribonacci3(int n) 
+    {
+        if(n==0 || n==1)
+            return n;
+        if(n==2)
+            return 1;
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
+     
+    // recursion with memoization
+    int tribonacci2(int n) 
+    {
+        vector<int> dp(n+1,-1);    
+        return helper(n,dp);
+    }
+    int helper(int n,vector<int> &dp) 
+    {
+        if(n==0 or n==1)
+            return n;
+        if(n==2)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+    }
     
-//     int tribonacci1(int n) 
-//     {
-//         if(n==0 || n==1)
-//             return n;
-//         if(n==2)
-//             return 1;
-//         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-//     }
-        
-//     int tribonacci(int n) 
-//     {
-//         vector<int> dp(n+1,-1);    
-//         return helper(n,dp);
-//     }
-//     int helper(int n,vector<int> &dp) 
-//     {
-//         if(n==0 or n==1)
-//             return n;
-//         if(n==2)
-//             return 1;
-//         if(dp[n]!=-1)
-//             return dp[n];
-//         return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
-//     }
-    
-    int tribonacci(int n) 
+    // tabulation
+    int tribonacci1(int n) 
     {
         if(n==0 || n==1)
             return n;
@@ -38,6 +40,16 @@ public:
         for(int i=3;i<=n;i++)
             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         return dp[n];
+    }
+    
+    // space optimization
+    int tribonacci(int n) 
+    {
+        vector<int> dp(4);
+        dp[1]=1,dp[2]=1;
+        for(int i=3;i<=n;i++)
+            dp[i%4]=dp[(i-1)%4]+dp[(i-2)%4]+dp[(i-3)%4];
+        return dp[n%4];
     }
     
 };
