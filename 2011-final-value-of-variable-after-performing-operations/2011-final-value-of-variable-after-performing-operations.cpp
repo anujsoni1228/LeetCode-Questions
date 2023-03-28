b41da9a997a2b25832192de7fef9cc590131765a
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-28 16:55:24 +0530

    Time: 6 ms (82.93%), Space: 14.1 MB (9.82%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -3,12 +3,7 @@ public:
     int finalValueAfterOperations(vector<string>& o) {
         int x=0;
         for(auto i:o)
-        {
-            if(i[1]=='+')
-                x++;
-            if(i[1]=='-')
-                x--;
-        }
+            i[1]=='+'?x++:x--;
         return x;
     }
 };
