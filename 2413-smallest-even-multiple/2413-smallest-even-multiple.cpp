ea43626376f82e6b7e2622b8749b82744287f040
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-29 15:16:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.43%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n%2==0)
+            return n;
+         if(n==1)
+             return 2;
+        for(int i=n;1000;i++)
+            if(i%2==0 and i%n==0)
+                return i;
+        return 0;
+    }
+};
