f19bbf09e672dcddee80b1447d19104ad3545407
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-29 16:24:51 +0530

    Time: 523 ms (92.00%), Space: 164 MB (64.22%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -1,23 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
+    // dono me sath me inorder traversal chalaya he p
     TreeNode* ans;
     TreeNode* inorder(TreeNode* o, TreeNode* c, TreeNode* t)
     {
         if(o)
         {
             inorder(o->left,c->left,t);
-            if(o==t)
-                ans=c;
+            if(o==t) ans=c;
             inorder(o->right,c->right,t);
         }
         return NULL;
