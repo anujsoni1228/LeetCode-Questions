9d120fa09b07eeeb3e58eb6e9c8aac1a936a3d59
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-29 15:27:16 +0530

    Time: 8 ms (18.80%), Space: 8.5 MB (46.90%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         int n=nums.size();
         for(int i=1;i<n;i++)
-        {
             nums[i]+=nums[i-1];
-        } 
         return nums;
     }
 };
