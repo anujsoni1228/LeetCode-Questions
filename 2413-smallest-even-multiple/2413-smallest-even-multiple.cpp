32a98758ac7a1b2d93ad66f8aee1455512b543d4
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-29 15:17:43 +0530

    Time: 3 ms (38.35%), Space: 6 MB (8.83%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -5,7 +5,7 @@ public:
             return n;
          if(n==1)
              return 2;
-        for(int i=n;;i++)
+        for(int i=n;154;i++)
             if(i%2==0 and i%n==0)
                 return i;
         return 0;
