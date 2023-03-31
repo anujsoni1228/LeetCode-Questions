ef0c29e59e3b9c58a9dc26b961cb74f57fed3f6c
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-31 18:48:08 +0530

    Time: 4 ms (37.22%), Space: 6.4 MB (45.26%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -5,10 +5,8 @@ public:
         string ans;
         int c=0;
         for(int i=0;i<s.size();i++)
-        {
             if(s[i]<='9')
                 x[s[i]-'0'-1]=s.substr(c,i-c),c=i+2,i++;
-        }
         for(auto i:x)if(i!="")ans+=i+' ';
         ans.pop_back();
         return ans;
