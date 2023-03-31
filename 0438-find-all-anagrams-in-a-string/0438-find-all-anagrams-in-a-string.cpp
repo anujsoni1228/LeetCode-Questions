2db65e68787dc4944e8611e029fd1aeb12e9bb32
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-31 17:14:16 +0530

    Time: 18 ms (44.66%), Space: 8.8 MB (20.61%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,52 +2,30 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) 
     {
-        // calculate the lengths of each string
-        int m=s.size();
-        int n=p.size();
-
-        // if p's length is more than s (do nothing)
-        if(n>m)
-            return {};
-
-        // build two hash sets 
-        // 1. for p (contain frequencies of each character in p) 
-        // 2. for s (contain frequencies of each character in current window)
-        int h[26]={0};
-        int ph[26]={0};
-        
-        // for ans
+        int i=0,j=0;
+        unordered_map<int,int> m;
         vector<int> ans;
-
-        //count the frequencies of first match
-        // e.g. - "abc" and "cba"-"ebabacd" have been counted
-        for(int i=0;i<n;i++)
-            ph[p[i]-'a']++,h[s[i]-'a']++;
-        
-        // sliding window approach
-        for(int i=0;i+n-1<m;)
+        for(auto i:p)
+            m[i]++;
+        int count=m.size();
+        int k=p.size();
+        while(j<s.size())
         {
-            bool f=0;
-            // chck for beinig anagram (by comparing frequencies of char)
-            // if atleast a single value doest match then its not our ans
-            for(int j=0;j<26;j++)
-                if(ph[j]!=h[j])
-                    {f=1;break;}
-
-            // if it's ans then store it
-            if(!f)
-                ans.push_back(i);
-
-            // decrease the frequencies of char removed from window
-            h[s[i]-'a']--;
-            // i++ means slide the window to right
-            i++;
-            // (edge case handle + increase the count 
-            // of char being included to window)
-            if(s[i+n-1]!='\0')
-            h[s[i+n-1]-'a']++;            
+            m[s[j]]--;
+            if(m[s[j]]==0)
+                count--;
+            if(j-i+1==k)
+            {
+                if(count==0) 
+                    ans.push_back(i);
+                m[s[i]]++;
+                if(m[s[i]]==1)
+                    count++;
+                i++,j++;
+            }
+            else
+                j++;
         }
         return ans;
     }
-
 };
