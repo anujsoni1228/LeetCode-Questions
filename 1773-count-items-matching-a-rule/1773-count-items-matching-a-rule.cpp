8d9b5fd927e85b6348a2c2be5eb49b4dd368c8aa
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-03-31 18:25:32 +0530

    Time: 80 ms (50.64%), Space: 36.5 MB (8.63%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string rk, string rv) {
         int c=0,o;
-        if(rk[0]=='t')
-            o=0;
-        else if(rk[0]=='c')
-            o=1;
-        else
-            o=2;
-        for(auto i:items)
-            c+=i[o]==rv;
+        o=rk[0]=='t'?0:rk[0]=='c'?1:2;
+        for(auto i:items)c+=i[o]==rv;
         return c;
     }
 };
