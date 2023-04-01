7c89fed1161a8337d095e1e510a574baba218967
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-01 20:11:44 +0530

    Time: 285 ms (84.98%), Space: 134.8 MB (42.53%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // my own approach code using map+pq
     vector<int> maxSlidingWindow1(vector<int>& nums, int k) {
         vector<int> ans;
         priority_queue<int> pq;
@@ -21,6 +22,26 @@ public:
         return ans;
     }
     
+    // actual solution
+    
+//     Create a Deque, d of capacity K, that stores only useful elements of current window of K elements. An element is useful if it is in current window and is greater than all other elements on right side of it in current window. Process all array elements one by one and maintain d to contain useful elements of current window and these useful elements are maintained in sorted order. The element at front of the d is the largest and element at rear/back of d is the smallest of current window.
+        
+// Follow the given steps to solve the problem:
+
+// Create a deque to store K elements.
+    
+// Run a loop and insert the first K elements in the deque. Before inserting the element, check if the element at the back of the queue is smaller than the current element, if it is so remove the element from the back of the deque until all elements left in the deque are greater than the current element. Then insert the current element, at the back of the deque.
+    
+// Now, run a loop from K to the end of the array.
+    
+// Print the front element of the deque.
+    
+// Remove the element from the front of the queue if they are out of the current window
+    
+// Insert the next element in the deque. Before inserting the element, check if the element at the back of the queue is smaller than the current element, if it is so remove the element from the back of the deque until all elements left in the deque are greater than the current element. Then insert the current element, at the back of the deque.
+    
+// Print the maximum element of the last window.
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
         deque<int> d;
