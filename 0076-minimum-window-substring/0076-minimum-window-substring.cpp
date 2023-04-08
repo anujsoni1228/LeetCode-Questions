d4db4d5faabc7447a4f5929168b6a932352c50d7
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-08 15:38:00 +0530

    Time: 27 ms (53.88%), Space: 7.7 MB (95.95%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) 
+    {
+        // if(s.size()<t.size())
+        //     return "";
+        unordered_map<char,int> m;
+        int c=0,i=0,j=0,n=s.size(),ans=INT_MAX,a=0,b=s.size()-1;
+        for(auto i:t)m[i]++;
+        c=m.size();
+        while(j<n)
+        {
+            if(m.find(s[j])!=m.end())
+            {   
+                m[s[j]]--;
+                if(m[s[j]]==0)
+                c--;
+            }
+            while(c==0)
+            {
+                if(j-i+1<ans)
+                    ans=j-i+1,a=i,b=j;
+                if(m.find(s[i])!=m.end())
+                {
+                    m[s[i]]++;
+                    if(m[s[i]]>0)
+                        c++;    
+                }
+                i++;                    
+            }
+            j++;
+        }
+        if(ans!=INT_MAX)
+            return s.substr(a,b-a+1);
+        return "";
+    }
+};
