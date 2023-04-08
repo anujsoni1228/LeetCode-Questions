7266351272340b78ebd0fc5a234fbb1364ee2a3b
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-08 15:50:28 +0530

    Time: 31 ms (44.84%), Space: 7.9 MB (71.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+// sliding window toughest question
+// agar ye krliya to sab krlega tu 
+// by aditya verma (paaylist lats question)
+    
+    
+// Make a frequency map of t
+// First store the frequency of every character of t string.
+// Then loop over the string and decrement the frequency from the map
+// When count variable equals to zero then start minimizing the window.
+    
     string minWindow(string s, string t) 
     {
         unordered_map<char,int> m;
@@ -32,4 +42,10 @@ public:
             return s.substr(a,b-a+1);
         return "";
     }
+    
+// HINTS
+// Use two pointers to create a window of letters in s, which would have all the characters from t.
+// Expand the right pointer until all the characters of t are covered.
+// Once all the characters are covered, move the left pointer and ensure that all the characters are still covered to minimize the subarray size.
+// Continue expanding the right and left pointers until you reach the end of s.
 };
