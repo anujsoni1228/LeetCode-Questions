4bfd44e69767697f1c6755008bc4b276f618498f
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-09 15:33:49 +0530

    Time: 11 ms (39.64%), Space: 10.1 MB (81.17%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -3,9 +3,15 @@ public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& g) {
         int n=g.size(),ans=0;
         vector<int> r(n),c(n);
+        
+        // har row ka maximum r[i] and har col ka max c[i]
+        // nikal ke store krlo
         for(int i=0;i<n;i++)
             for(int j=0;j<n;j++)
-                r[i]=max(r[i],g[i][j]),c[j]=max(c[j],g[i][j]);  
+                r[i]=max(r[i],g[i][j]),c[j]=max(c[j],g[i][j]);
+        
+        // har [i][j]th element ith row ke max or jth col k max
+        // dono ke minimum ko cross nhi kr sktae
         for(int i=0;i<n;i++)
             for(int j=0;j<n;j++)
                 ans+=min(r[i],c[j])-g[i][j];
