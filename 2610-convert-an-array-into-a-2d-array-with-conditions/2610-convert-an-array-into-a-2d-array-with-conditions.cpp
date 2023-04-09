a58056c6d12e143fd853c29923492de5915eb5e5
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-09 15:58:11 +0530

    Time: 24 ms (24.19%), Space: 29.5 MB (90.85%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // straight forward solution
+    // find freq of all elements and then 
+    // fill them accordingly
     vector<vector<int>> findMatrix(vector<int>& n) {
         unordered_map<int,int> m;
         int mx=INT_MIN;
@@ -7,9 +10,7 @@ public:
         vector<vector<int>> ans(mx);
         for(auto i:m)
             while(i.second-->0)
-                ans[i.second].push_back(i.first);
-            
-        
+                ans[i.second].push_back(i.first);           
         return ans;
     }
 };
