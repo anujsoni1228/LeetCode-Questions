5128101df7a153112a930e52bacde546f0fa3d33
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-04-11 18:17:30 +0530

    Added solution - LeetHub

---
+++ Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void helper(stack<int> &s, int t)
+    {
+        if(t==0)
+        {
+            s.pop();
+            return;
+        }
+        t--;
+        int x=s.top();
+        s.pop();
+        helper(s,t);
+        s.push(x);
+    }
+    void deleteMid(stack<int> &s, int sz)
+    {
+        helper(s,sz-(sz+1)/2);   
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
