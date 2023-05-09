4af0fe5fdde0a7331a8cdbd6cc6b71039b050b1a
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-09 14:51:52 +0530

    Time: 120 ms (78.43%), Space: 76 MB (88.24%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -7,4 +7,16 @@ public:
             ans[i]=p[i]^p[i-1];
         return ans;            
     }
+    
+//     let the array be a,b,c,d,e
+//     a         =p[0]
+//     a^b       =p[1]
+//     a^b^c     =p[2]
+//     a^b^c^d   =p[3]
+//     a^b^c^d^e =p[4]
+        
+//     a=p[0]
+//     b=p[1]^p[0]
+//     c=p[2]^p[1]
+//     and so on...
 };
