fca992b7948706c537ed9af12357add75bb0bdb6
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-18 23:02:26 +0530

    Time: 184 ms (33.74%), Space: 16.2 MB (71.24%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    
     vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
         int c;
         vector<int> ans;
         for (int i = 0; i < queries.size(); i++) {
             c = 0;
             for (int j = 0; j < points.size(); j++) {
+                // Here it is a brute force solution in which I simply have used the equation of circle that is X^2 + Y^2 <= R^2
                 if (pow(points[j][0] - queries[i][0], 2) + pow(points[j][1] - queries[i][1], 2) <= pow(queries[i][2], 2))
                     c++;
             }
