81a726c180c7d3a76a6ef9934fd87a3aee023100
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-19 21:48:02 +0530

    Time: 134 ms (17.58%), Space: 35.1 MB (42.23%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    /* imp observation
+        
+    full binary tree (FBT) always has an odd number of nodes.
+    
+    checks if 'n' is even, and if it is, immediately return an empty vector since it's not possible to create a full binary tree with an even number of nodes
+    
+    If 'n' is odd, we initialize an empty vector 'ans' to store the generated FBTs.
+
+    We iterate from 2 to 'n+1' with a step size of 2 (to maintain ODD wala observation). We use 'i' to split the nodes into the left and right subtrees.
+        
+    n=7
+    -----------------------------------
+    
+        1  2  3  4  5  6  7  8  9 
+           ↑      
+           
+        1  2  3   4   5  6  7  8  9 
+        <----->   ↑  <------------>   
+        (left    (i)     (right
+        subtree)         subtree)
+                 
+        1  2  3  4  5  6  7  8  9 
+                       ↑      
+        1  2  3  4  5  6  7  8  9 
+                             ↑ 
+
+    For each 'i', we recursively call the 'allPossibleFBT' function to generate all possible left subtrees with 'i-1' nodes.
+
+    We also recursively call the 'allPossibleFBT' function to generate all possible right subtrees with 'n-i' nodes.
+
+    We then iterate through each combination of left and right subtrees and create a new TreeNode object with value 0.
+
+    We assign the left and right subtrees to the newly created node and add it to the 'ans' vector.
+    
+    Finally, we return the 'ans' vector containing all the generated FBTs.
+*/
+
+    
     vector<TreeNode*> allPossibleFBT(int n) {
         if(n==1)
             return {new TreeNode(0)};
