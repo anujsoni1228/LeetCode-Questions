d920977b3db53d73de2f572561c118c0371247b7
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-19 20:18:14 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -17,23 +17,42 @@ class Solution
     //     return knapSack(w,wt,val,n-1);
     // }
     
+    // int helper(int w, int wt[], int val[], int n,vector<vector<int>> &dp)
+    // {
+    //     if(n==0 or w==0)
+    //         return 0;
+    //     if(dp[w][n]!=-1)
+    //         return dp[w][n];
+    //     if(wt[n-1]<=w)
+    //         return dp[w][n]=max(val[n-1]+helper(w-wt[n-1],wt,val,n-1,dp),helper(w,wt,val,n-1,dp));
+    //     return dp[w][n]=helper(w,wt,val,n-1,dp);
+    // }
     
-    int helper(int w, int wt[], int val[], int n,vector<vector<int>> &dp)
-    {
-        if(n==0 or w==0)
-            return 0;
-        if(dp[w][n]!=-1)
-            return dp[w][n];
-        if(wt[n-1]<=w)
-            return dp[w][n]=max(val[n-1]+helper(w-wt[n-1],wt,val,n-1,dp),helper(w,wt,val,n-1,dp));
-        return dp[w][n]=helper(w,wt,val,n-1,dp);
-    }
+    // int knapSack(int w, int wt[], int val[], int n) 
+    // { 
+    //     vector<vector<int>> dp(w+1,vector<int> (n+1,-1));
+    //     return helper(w,wt,val,n,dp);
+    // }
     
     int knapSack(int w, int wt[], int val[], int n) 
     { 
-        vector<vector<int>> dp(w+1,vector<int> (n+1,-1));
-        return helper(w,wt,val,n,dp);
+        vector<vector<int>> dp(w+1,vector<int> (n+1,0));
+        for(int i=1;i<=w;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(wt[j-1]<=i)
+                    dp[i][j]=max(val[j-1]+dp[i-wt[j-1]][j-1],dp[i][j-1]);
+                else  
+                    dp[i][j]=dp[i][j-1];
+            }
+        }
+        return dp[w][n];
     }
+    
+    
+    
+    
 };
 
 //{ Driver Code Starts.
