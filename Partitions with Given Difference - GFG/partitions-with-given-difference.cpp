5885f0bafb1f9cfdbf7983d9bb4e6136c35ed79e
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-21 16:17:34 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -5,43 +5,43 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // int countPartitions(int n, int d, vector<int>& arr) {
-    //     int sum=0;
-    //     for(auto i:arr)sum+=i;
-    //     if((d+sum)%2==1)return 0;
-    //     vector<vector<int>> dp(n+1,vector<int> (sum+1,0));
-    //     dp[0][0]=1;
-    //     for(int i=1;i<n+1;i++)
-    //     {
-    //         for(int j=0;j<sum+1;j++)
-    //         {
-    //             if(j-arr[i-1]>=0)
-    //                 dp[i][j]=(dp[i-1][j-arr[i-1]] + dp[i-1][j])%1000000007;
-    //             else 
-    //                 dp[i][j]=dp[i-1][j];
-    //         }
-    //     }
-    //     return dp[n][(d+sum)/2]%1000000007;
-        
-    // }
     int countPartitions(int n, int d, vector<int>& arr) {
         int sum=0;
-        for(auto i: arr) sum+=i;
+        for(auto i:arr)sum+=i;
         if((d+sum)%2 != 0) return 0;
         int t=(d+sum)/2;
-        //REDUCED: find count of subset with sum equal to t
-        vector<vector<int>> dp(n+1, vector<int>(t+1, 0));
-        dp[0][0] = 1;
-        for(int i=1; i<n+1; i++) {
-            for(int j=0; j<t+1; j++) {
-                if(arr[i-1] <= j) 
-                    dp[i][j] = (dp[i-1][j] + dp[i-1][j-arr[i-1]])%1000000007;
-                else
-                    dp[i][j] = dp[i-1][j];
+        vector<vector<int>> dp(n+1,vector<int> (t+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<t+1;j++)
+            {
+                if(arr[i-1] <= j)
+                    dp[i][j]=(dp[i-1][j-arr[i-1]] + dp[i-1][j])%1000000007;
+                else 
+                    dp[i][j]=dp[i-1][j];
             }
         }
         return dp[n][t];
     }
+    // int countPartitions(int n, int d, vector<int>& arr) {
+    //     int sum=0;
+    //     for(auto i: arr) sum+=i;
+    //     if((d+sum)%2 != 0) return 0;
+    //     int t=(d+sum)/2;
+    //     //REDUCED: find count of subset with sum equal to t
+    //     vector<vector<int>> dp(n+1, vector<int>(t+1, 0));
+    //     dp[0][0] = 1;
+    //     for(int i=1; i<n+1; i++) {
+    //         for(int j=0; j<t+1; j++) {
+    //             if(arr[i-1] <= j) 
+    //                 dp[i][j] = (dp[i-1][j] + dp[i-1][j-arr[i-1]])%1000000007;
+    //             else
+    //                 dp[i][j] = dp[i-1][j];
+    //         }
+    //     }
+    //     return dp[n][t];
+    // }
 };
 
 //{ Driver Code Starts.
