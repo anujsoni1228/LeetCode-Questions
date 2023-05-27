4bb9f4ec9cfc6f88fdaeb5ab681597e318ec3369
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-27 22:25:17 +0530

    Time: 26 ms (54.67%), Space: 22.1 MB (8.30%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int change(int a, vector<int>& c) 
+    int helper(int a,vector<int>& c,int n,vector<vector<int>> &dp)
     {
-        vector<vector<int>> dp(c.size()+1, vector<int>(a+1));
-        // for(int i=0;i<=a;i++)
-        //     dp[0][i]=0;        
-        for(int i=0;i<=c.size();i++)
-            dp[i][0]=1;
-        for(int i=1;i<=c.size();i++)
-        {
-            for(int j=1;j<=a;j++)
-            {
-                if(j-c[i-1]>=0)
-                    dp[i][j]=dp[i][j-c[i-1]]+dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[c.size()][a];                  
+        if(a==0)
+            return 1;
+        if(n==0)
+            return 0;
+        if(dp[a][n]!=-1)
+            return dp[a][n];
+        if(a-c[n-1]>=0)
+            return dp[a][n]=helper(a-c[n-1],c,n,dp)+helper(a,c,n-1,dp);  
+        return dp[a][n]=helper(a,c,n-1,dp);  
+    }
+    int change(int a, vector<int>& c) {
+        vector<vector<int>> dp(a + 1, vector<int>(c.size()+1, -1));     
+        return helper(a,c,c.size(),dp);
     }
 };
