28bb92c6ac250f5421fb3b5667027c503d0c915e
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-05-30 15:08:23 +0530

    Added solution - LeetHub

---
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    int lcs(int x, int y, string s1, string s2)
+    {
+        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[x][y];
+    }
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        int x=lcs(m,n,X,Y);
+        return m+n-x;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{   
+    
+    int t;
+    
+    //taking total testcases
+    cin >> t;
+    while(t--){
+    string X, Y;
+    //taking String X and Y
+	cin >> X >> Y;
+	
+	//calling function shortestCommonSupersequence()
+	Solution obj;
+	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
