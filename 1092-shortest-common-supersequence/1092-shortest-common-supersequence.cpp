323b8fef9d8cacdb820ed59383dfea056a6c05c9
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-06-03 14:42:11 +0530

    Time: 29 ms (60.34%), Space: 13 MB (21.82%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string lcs1(int x, int y, string s1, string s2) {
         string ans;
+        // Creating a 2D vector for dynamic programming
         vector<vector<int>> dp(x + 1, vector<int>(y + 1, 0));
 
+        // Finding the length of the longest common subsequence
         for (int i = 1; i <= x; i++) 
         {
             for (int j = 1; j <= y; j++) 
@@ -15,6 +17,7 @@ public:
             }
         }
 
+        // Constructing the longest common subsequence
         int i = x, j = y;
         while (i > 0 && j > 0) 
         {
@@ -33,6 +36,7 @@ public:
             }
         }
 
+        // Reversing the longest common subsequence
         reverse(ans.begin(), ans.end());
         return ans;
     }
@@ -41,6 +45,9 @@ public:
         string lcs = lcs1(a.size(), b.size(), a, b);
         string ans;
 
+        // The three-pointer logic finds the shortest common supersequence of strings a and b. It iterates through a, b, and the longest common subsequence (LCS). If characters match in a and b, and also with the LCS, they are added to the supersequence. If characters match with only one of them, the unmatched character is added. If no match occurs, both characters are added. Finally, remaining characters from a and b are appended. This logic efficiently combines characters, using the LCS as a reference to avoid duplicates.
+        
+        // Constructing the shortest common supersequence
         int i = 0, j = 0, k = 0;
         while (i < a.size() && j < b.size() && k < lcs.size()) 
         {
@@ -76,17 +83,20 @@ public:
             }
         }
 
+         // Appending remaining characters from string a
         while (i < a.size()) 
         {
             ans += a[i];
             i++;
         }
 
+        // Appending remaining characters from string b
         while (j < b.size())
         {
             ans += b[j];
             j++;
         }
+        // Returning the shortest common supersequence
         return ans;
     }
 };
