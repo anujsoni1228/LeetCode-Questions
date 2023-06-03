6f0590ea8699b24ade53b3a2525f74b701fac33b
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-06-03 14:30:29 +0530

    Time: 37 ms (36.62%), Space: 12.8 MB (64.91%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    string lcs1(int x, int y, string s1, string s2)
+    {
+        string ans;
+        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int i=x,j=y;
+        while(i>0&&j>0)s1[i-1]==s2[j-1]?ans+=s1[i-1],i--,j--:dp[i-1][j]>dp[i][j-1]?i--:j--;
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    
+    string shortestCommonSupersequence(string a, string b) {
+        string lcs=lcs1(a.size(),b.size(),a,b),ans;  
+        int i=0,j=0,k=0;
+        while(i<a.size() and j<b.size() and k<lcs.size())
+        {
+            if(a[i]==b[j])
+            {
+                if(a[i]==lcs[k])
+                {
+                    ans+=a[i];
+                    i++;
+                    j++;
+                    k++;
+                }
+            }
+            else
+            {
+                if(a[i]==lcs[k])
+                {
+                    ans+=b[j];
+                    j++;
+                }
+                else if(b[j]==lcs[k])
+                {
+                    ans+=a[i];
+                    i++;
+                }
+                else
+                {
+                    ans+=a[i];
+                    ans+=b[j];
+                    i++;
+                    j++;
+                }
+            }
+        }
+        while(i<a.size() and j<b.size())
+        {
+            ans+=a[i];
+            ans+=b[j];
+            i++;
+            j++;
+        }
+        while(i<a.size())
+        {
+            ans+=a[i];
+            i++;
+        }
+        while(j<b.size())
+        {
+            ans+=b[j];
+            j++;
+        }
+        cout<<lcs;
+        return ans;
+    }
+};
