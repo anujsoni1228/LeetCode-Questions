a1d27f176e612e588f043fbda1551ccfbae77b73
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-06-09 15:24:22 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -9,53 +9,44 @@ using namespace std;
 
 class Solution{
 public:
-     vector<vector<pair<int,int>>> dp;
-    pair<int,int> ways(string &s, int i, int j){
-        if(i > j) return {0,0};
-        if(i == j){
-            if(s[i] == 'T') 
-                return {1, 0};
-            else 
-                return {0, 1};
-        }
-        
-        if(dp[i][j].first != -1) return dp[i][j];
-        
-        int trueCount=0;
-        int falseCount=0;
-        for(int k = i+1 ; k <= j ; k += 2){
-            pair<int,int> leftCount = ways(s, i, k - 1); //left
-            pair<int,int> rightCount = ways(s, k + 1, j); // right
-            
-            int lTrue = leftCount.first;
-            int lFalse = leftCount.second;
-            int rTrue = rightCount.first;
-            int rFalse = rightCount.second;
-            
-            if(s[k] == '&'){
-                trueCount += lTrue * rTrue;
-                falseCount += (lFalse * rFalse + lFalse * rTrue + lTrue * rFalse);
-            }else if(s[k] == '|'){
-                trueCount += (lTrue * rFalse + lFalse * rTrue + lTrue * rTrue);
-                falseCount += (lFalse * rFalse);
-            }else{
-                trueCount += (lFalse * rTrue + lTrue * rFalse);
-                falseCount += (lFalse * rFalse + lTrue * rTrue);
+    pair<int,int> solve(string s,int i,int j,vector<vector<pair<int,int>>> &dp)
+    {
+        if(i==j)
+            return dp[i][j]={s[i]=='T',s[i]=='F'};
+        if(dp[i][j].first!=-1 and dp[i][j].second!=-1)
+            return dp[i][j];
+        pair<int,int> ans={0,0};
+        for(int k=i+1;k<=j;k+=2)
+        {
+            pair<int,int> l=solve(s,i,k-1,dp);
+            pair<int,int> r=solve(s,k+1,j,dp);
+            switch(s[k])
+            {
+                case '&':
+                    ans.first+=( l.first*r.first)%1003;
+                    ans.second+=((l.first*r.second)%1003 + (l.second*r.first)%1003 + (l.second*r.second)%1003)%1003;
+                break;
+                case '|':
+                    ans.first+=( (l.first*r.second)%1003 + (l.second*r.first)%1003 + (l.first*r.first)%1003)%1003;
+                    ans.second+=(l.second*r.second)%1003;
+                break;
+                case '^':
+                   ans.first+=( (l.first*r.second)%1003 + (l.second*r.first)%1003)%1003;
+                   ans.second+=((l.first*r.first)%1003+(l.second*r.second)%1003)%1003;
+                break;
             }
-            trueCount %= 1003;
-            falseCount %= 1003;
+            ans.first%=1003;
+            ans.second%=1003;
         }
-        
-        return dp[i][j] = {trueCount, falseCount};
-    } 
-
-    int countWays(int N, string S){
-        dp.resize(N + 1, vector<pair<int,int>>(N + 1, {-1,-1}));
-        return ways(S,0, N-1).first;
+        return dp[i][j]= {ans.first, ans.second};
     }
-
-
-
+    
+    int countWays(int n, string s)
+    {
+        vector<vector<pair<int,int>>> dp(n,vector<pair<int,int>>(n,{-1,-1}));
+        pair<int,int> ans=solve(s,0,n-1,dp);
+        return ans.first;
+    }
 };
 
 //{ Driver Code Starts.
