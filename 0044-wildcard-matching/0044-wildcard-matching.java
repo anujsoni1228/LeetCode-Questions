a9e648dd0e604d4f23c663dfe86cf0924779e733
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-06-30 17:39:48 +0530

    Time: 2 ms (100.00%), Space: 43.6 MB (73.28%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,29 +1,41 @@
 class Solution {
+// greedy solution with idea of DFS
+// starj stores the position of last * in p
+// last_match stores the position of the previous matched char in s after a *
+// e.g. 
+// s: a c d s c d
+// p: * c d
+// after the first match of the *, starj = 0 and last_match = 1
+// when we come to i = 3 and j = 3, we know that the previous match of * is actually wrong, 
+// (the first branch of DFS we take is wrong)
+// then it resets j = starj + 1 
+// since we already know i = last_match will give us the wrong answer
+// so this time i = last_match+1 and we try to find a longer match of *
+// then after another match we have starj = 0 and last_match = 4, which is the right solution
+// since we don't know where the right match for * ends, we need to take a guess (one branch in DFS), 
+// and store the information(starj and last_match) so we can always backup to the last correct place and take another guess.
 
-    public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        dp[s.length()][p.length()] = true;
+    public boolean isMatch(String s1, String p1) {
+        char[] s = s1.toCharArray();
+        char[] p = p1.toCharArray();
 
-        // this for loop for initialization is very impoprtant for this code
-        // p me piche se jitne bhi stars he (lagatar wale un sabko 1 se
-        //  initialize krna pdega ) in last row of dp baki sab thik h 
-        for (int j = p.length() - 1; j >= 0; j--) {
-            if (p.charAt(j) == '*') {
-                dp[s.length()][j] = true;
+        int i = 0, j = 0, starj = -1, lastMatch = -1;
+        while (i < s.length) {
+            if (j < p.length && (s[i] == p[j] || p[j] == '?')) {
+                i++;
+                j++;
+            } else if (j < p.length && p[j] == '*') {
+                starj = j;
+                lastMatch = i;
+                j++;
+            } else if (starj != -1) {
+                j = starj + 1;
+                i = ++lastMatch;
             } else {
-                break;
+                return false;
             }
         }
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = p.length() - 1; j >= 0; j--) {
-                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
-                    dp[i][j] = dp[i + 1][j + 1];
-                } else if (p.charAt(j) == '*') {
-                    dp[i][j] = dp[i + 1][j] || dp[i][j + 1];
-                }
-            }
-        }
-        return dp[0][0];
+        while (j < p.length && p[j] == '*') j++;
+        return j == p.length;
     }
 }
