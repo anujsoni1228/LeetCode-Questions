164aeecb88f8622c11c5bab4bbc14a6973d53f60
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-07-10 21:20:23 +0530

    Time: 7 ms (36.18%), Space: 43.6 MB (77.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,5 +1,20 @@
 class Solution {
 
+// How is Matrix built :
+// The matrix is built iteratively using the following recurrence relation:
+    
+// If word1[i-1] == word2[j-1], then dp[i][j] = dp[i-1][j-1]. That is, no operation is required because the characters at positions i-1 and j-1 are already the same.
+    
+// Otherwise, dp[i][j] is the minimum of the following three values:
+    
+// dp[i-1][j-1] + 1: replace the character at position i-1 in word1 with the character at position j-1 inword2.
+// dp[i-1][j] + 1: delete the character at position i-1 in word1.
+// dp[i][j-1] + 1: insert the character at position j-1 in word2 into word1 at position i.
+    
+// The base cases are:
+// dp[i][0] = i: transforming word1[0...i-1] into an empty string requires i deletions.
+// dp[0][j] = j: transforming an empty string into word2[0...j-1]requires j insertions.
+    
     public int minDistance(String a, String b) {
         int[][] dp = new int[a.length() + 1][b.length() + 1];
         for (int i = 0; i <= a.length(); i++) {
