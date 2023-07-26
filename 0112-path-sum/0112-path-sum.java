75ee2bfb18fb08ce9c9532a9eb3f71721db72406
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-07-26 16:26:35 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (16.62%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -3,11 +3,10 @@ class Solution {
 
     private void has(TreeNode r, int s, int t) {
         if (r == null) return;
-        if (r.left == null && r.right == null) {
-            if (!ans) ans = (s + r.val == t);
-        }
-        if (r.left != null) has(r.left, s + r.val, t);
-        if (r.right != null) has(r.right, s + r.val, t);
+        s += r.val;
+        if (r.left == null && r.right == null && !ans) ans = (s == t);
+        if (r.left != null) has(r.left, s, t);
+        if (r.right != null) has(r.right, s, t);
     }
 
     public boolean hasPathSum(TreeNode r, int t) {
