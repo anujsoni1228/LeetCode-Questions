ea2f6e3cc35624fa7c116a730eee1239c011ec20
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-07-31 17:38:08 +0530

    Time: 2 ms (45.93%), Space: 5.9 MB (74.48%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+        string convertToTitle(int n)
+        {
+            string ans;
+            while (n)
+            {
+                n--;
+                ans = (char)(n % 26 + 65) + ans;
+                n /= 26;
+            }
+            return ans;
+        }
+};
