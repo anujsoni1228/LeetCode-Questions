df51cb2f853514fdb4a49cefc082a51be8db3c45
Anuj Soni <120590255+anujsoni1228@users.noreply.github.com>
2023-08-10 22:54:10 +0530

    Time: 17 ms (93.02%), Space: 59.5 MB (14.73%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
@@ -1,5 +1,21 @@
 class Solution {
+// In the given code, the algorithm uses binary search to find the minimum maximum difference that can be achieved when forming `p` pairs of indices from the sorted `nums` array. The goal is to minimize this maximum difference among all possible pairs.
 
+// Here's why the code returns `l` instead of `r`:
+
+// 1. `l` and `r` are two pointers used to bound the possible range of the minimum maximum difference. Initially, `l` starts from 0 (minimum possible difference) and `r` starts from the maximum difference between the largest and smallest elements in the sorted array (`nums[nums.length - 1] - nums[0]`).
+
+// 2. The binary search iteratively tries to find the best value for the minimum maximum difference. It calculates the middle value `m` between `l` and `r` and then checks if it's possible to form `p` pairs with a maximum difference less than or equal to `m`.
+
+// 3. The inner loop iterates through the sorted array, comparing adjacent elements and checking if the difference between them (`nums[i] - nums[i - 1]`) is less than or equal to the current middle value `m`. If it is, it means we can consider this pair in our count, and we increment both `i` and `k`.
+
+// 4. The binary search adjusts the pointers based on the value of `k` after the inner loop. If `k` equals `p`, it means we were able to find `p` pairs with a maximum difference less than or equal to `m`, and we try to find a smaller value for the minimum maximum difference by adjusting the right pointer `r` to `m - 1`.
+
+// 5. If `k` is less than `p`, it means we couldn't find `p` pairs with a maximum difference less than or equal to `m`, and we need to increase the minimum maximum difference. Therefore, we adjust the left pointer `l` to `m + 1`.
+
+// 6. The binary search continues until `l` is greater than `r`, at which point it has found the smallest possible value for the minimum maximum difference, and this value is stored in `l`.
+
+// Since `l` represents the smallest value for the minimum maximum difference that can be achieved with `p` pairs of indices, the function returns `l`. It's worth noting that `r` could also be a valid answer, but it might not always represent the smallest value, so we choose the smaller value between `l` and `r`.
     public int minimizeMax(int[] nums, int p) {
         Arrays.sort(nums);
         int l = 0, r = nums[nums.length - 1] - nums[0];
